c39f7cdd8cc02484a1e5b9a72f9639258b42c659
Carlos Mendez <calicci.gardachi@gmail.com>
2015-07-17 02:36:49 +0000

    YELP Dev v0.4 Dynamic Page Titles

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-var yelp = require("yelp").createClient({
-    consumer_key        : "7jcglOo2SxF1laGI5iG8ow",
-    consumer_secret     : "Hex0_XLTxe-Hem8CTXMS1KFXRrw",
-    token               : "7zFN8ztVIXXw5Z16y3dhXvirWf-_O2pD",
-    token_secret        : "124GcqDYRmUtzF-Tf9EtQseRoOo"
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Penny Pincher' });
+    var title = 'Penny Pincher';
+    res.render('index', { title: title });
 });
 
-router.get('/welcome', function(req, res, next) {
-  res.render('welcome', { title: 'Welcome' });
-});
-
+/* GET privacy page. */
 router.get('/privacy', function(req, res, next) {
-  res.render('privacy', { title: 'Privacy' });
+    var title = 'Privacy'
+    res.render('privacy', { title: title });
 });
 
+/* GET terms of service page. */
 router.get('/tos', function(req, res, next) {
-  res.render('tos', { title: 'Terms of Service' });
+    var title = 'Terms of Service'
+    res.render('tos', { title: title });
 });
 
+/* GET support page. */
 router.get('/support', function(req, res, next) {
-  res.render('support', { title: 'Support' });
+    var title = 'Support';
+    res.render('support', { title: title });
 });
 
+/* GET search results page. */
 router.get('/search', function(req, res, next) {
+    var title = 'Results';
+    var yelp = require("yelp").createClient({
+        consumer_key        : "7jcglOo2SxF1laGI5iG8ow",
+        consumer_secret     : "Hex0_XLTxe-Hem8CTXMS1KFXRrw",
+        token               : "7zFN8ztVIXXw5Z16y3dhXvirWf-_O2pD",
+        token_secret        : "124GcqDYRmUtzF-Tf9EtQseRoOo"
+    });
     
-     yelp.search({term: "food", location: "Montreal"}, function(error, data) {
-    console.log(error);
+    yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+        console.log(error);
         
-          res.render('search', { title: 'Search', data: data });
+        res.render('search', { 
+            title           : title, 
+            data            : data 
+        });
     });
     
 });
